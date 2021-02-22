b29318e7f25cc76dbbde3c2a5bb964e0cf560aa2
Chiramet Phong Penglerd <Phong@hotmail.ch>
2021-02-22 13:41:37 +0100

    init nebular

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import versions from 'src/_versions';
+import {NbThemeService} from '@nebular/theme';
 
 @Component({
   selector: 'r-root',
@@ -7,6 +8,13 @@ import versions from 'src/_versions';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'resourcery';
   versions = versions;
+  theme = 'dark';
+
+  constructor(private nbThemeService: NbThemeService) {
+  }
+
+  changeTheme($event: string): void {
+    this.nbThemeService.changeTheme($event);
+  }
 }
