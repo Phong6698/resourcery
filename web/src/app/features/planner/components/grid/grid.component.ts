6b1c2219f04ef311495d475717788ff2f1567e21
Miro Zlatkovic <miroljub.zlatkovic@raiffeisen.ch>
2021-02-24 23:08:30 +0100

    wip:div wird nun absolut zum parent positioniert

--- web/src/app/features/planner/components/grid/grid.component.ts
+++ web/src/app/features/planner/components/grid/grid.component.ts
@@ -12,6 +12,8 @@ const interact: any = interactLib;
 })
 export class GridComponent implements OnInit {
 
+  days = new Array(31);
+
   constructor(
     private renderer: Renderer2
   ) {
@@ -23,21 +25,6 @@ export class GridComponent implements OnInit {
 
   @ViewChildren('renderedDays') renderedDays!: QueryList<ElementRef>;
 
-  months: any[] = [
-    new Month('Jan', 1, new Array(31)),
-    new Month('Feb', 2, new Array(31)),
-    new Month('Mar', 3, new Array(31)),
-    new Month('Apr', 4, new Array(31)),
-    new Month('May', 5, new Array(31)),
-    new Month('Jun', 6, new Array(31)),
-    new Month('Jul', 7, new Array(31)),
-    new Month('Aug', 8, new Array(31)),
-    new Month('Sep', 9, new Array(31)),
-    new Month('Oct', 10, new Array(31)),
-    new Month('Nov', 11, new Array(31)),
-    new Month('Dec', 12, new Array(31)),
-  ];
-
   ngOnInit(): void {
 
   }
@@ -47,23 +34,23 @@ export class GridComponent implements OnInit {
     console.log(event);
 
     // @ts-ignore
-    const rect = event.target?.getBoundingClientRect();
-
+    // const rect = event.target?.getBoundingClientRect();
+    const rect = event.target?.parentNode.getBoundingClientRect();
     console.log(rect);
 
     const d = this.renderer.createElement('div');
 
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    // debugger;
+
     this.renderer.addClass(d, 'draggable');
-    this.renderer.setStyle(d, 'left', rect.left + 'px');
-    this.renderer.setStyle(d, 'top', (rect.top + 6) + 'px');
+    // this.renderer.setStyle(d, 'left', rect.left + 'px');
+    // this.renderer.setStyle(d, 'top', (rect.top + 6) + 'px');
+    this.renderer.setStyle(d, 'left', x + 'px');
+    this.renderer.setStyle(d, 'top', 0 + 'px');
     // @ts-ignore
     this.renderer.appendChild(event.target?.parentNode, d);
-
-    const grid = this.makeGrid(event.target);
-
-
-    const position = { x: 0, y: 0 };
-
 
 
     interact(d)
@@ -94,9 +81,6 @@ export class GridComponent implements OnInit {
           }
         },
         modifiers: [
-          // interact.modifiers.restrictSize({
-          //   min: { width: 50, height: 50 }
-          // }),
           interact.modifiers.restrictEdges({
             outer: 'parent'
           }),
@@ -104,78 +88,23 @@ export class GridComponent implements OnInit {
             targets: this.makeGridXY(event.target),
             range: Infinity,
           }),
-          // interact.modifiers.snap({
-          //   targets: [
-          //     interact.snappers.grid(this.makeGridXY(event.target))
-          //   ],
-          //   range: Infinity,
-          //   relativePoints: [ { x: 0, y: 0 } ]
-          // }),
         ],
 
       })
       .draggable({
         listeners: { move: this.dragMoveListener },
-        // modifiers: [
-        //   // @ts-ignore
-        //   interact.modifiers.snapEdges({
-        //     targets: this.makeGrid(event.target)
-        //   })
-        // ],
-        // snap: {
-        //   targets: this.makeGrid(event.target)
-        // },
-
-        // enable inertial throwing
+        modifiers: [
+          interact.modifiers.snapEdges({
+            targets: this.makeGridXY(event.target),
+            range: Infinity,
+          }),
+          interact.modifiers.restrictRect({
+            restriction: 'parent'
+          })
+        ],
         inertia: false,
-        // keep the element within the area of it's parent
-        restrict: {
-          restriction: 'parent',
-          endOnly: true,
-          elementRect: {top: 0, left: 0, bottom: 1, right: 1}
-        },
-        // enable autoScroll
-
-        // // call this function on every dragmove event
-        // onmove: this.dragMoveListener,
-        // // call this function on every dragend event
-        // // tslint:disable-next-line:typedef no-shadowed-variable
-        // onend(event: any) {
-        //   const textEl = event.target.querySelector('p');
-        //
-        //   // tslint:disable-next-line:no-unused-expression
-        //   textEl && (textEl.textContent =
-        //     'moved a distance of '
-        //     // tslint:disable-next-line:no-bitwise
-        //     + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-        //     Math.pow(event.pageY - event.y0, 2) | 0))
-        //       .toFixed(2) + 'px');
-        // }
       });
-      // tslint:disable-next-line:no-shadowed-variable only-arrow-functions
-      // .on('resizemove', (event: any) => {
-      //   // tslint:disable-next-line:one-variable-per-declaration prefer-const
-      //   let target = event.target,
-      //     x = (parseFloat(target.getAttribute('data-x')) || 0),
-      //     y = (parseFloat(target.getAttribute('data-y')) || 0);
-      //
-      //   // update the element's style
-      //   target.style.width = event.rect.width + 'px';
-      //   target.style.height = event.rect.height + 'px';
-      //
-      //   // translate when resizing from top or left edges
-      //   x += event.deltaRect.left;
-      //   y += event.deltaRect.top;
-      //
-      //   target.style.webkitTransform = target.style.transform =
-      //     'translate(' + x + 'px,' + y + 'px)';
-      //
-      //   target.setAttribute('data-x', x);
-      //   target.setAttribute('data-y', y);
-      //   target.textContent = event.rect.width + '×' + event.rect.height;
-      // });
-    // @ts-ignore
-    console.log(event.target?.parentNode);
+
   }
 
   public jumpToday(): void {
@@ -202,20 +131,6 @@ export class GridComponent implements OnInit {
     target.setAttribute('data-y', y);
   }
 
-  private makeGrid(clickedDaySlot: EventTarget): { left, top }[] {
-    // @ts-ignore
-    const parent = clickedDaySlot.parentNode;
-    const targets = [];
-
-    [...parent.children].map((el: Element) => {
-      // @ts-ignore
-      const rect = el.getBoundingClientRect();
-      targets.push(interact.snappers.grid({left: rect.left}));
-    });
-    console.log(targets);
-    return targets;
-  }
-
   private makeGridXY(clickedDaySlot: EventTarget): { left, top }[] {
     // @ts-ignore
     const parent = clickedDaySlot.parentNode;
@@ -224,14 +139,10 @@ export class GridComponent implements OnInit {
     [...parent.children].map((el: Element) => {
       // @ts-ignore
       const rect = el.getBoundingClientRect();
-      targets.push(interact.snappers.grid({x: rect.left, y: rect.top}));
+      targets.push(interact.snappers.grid({x: rect.left, y: 1}));
     });
     console.log(targets);
     return targets;
   }
 }
 
-class Month {
-  constructor(public name: string, public ident: number, public days: any[]) {
-  }
-}
