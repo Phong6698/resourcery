eb6f1e408c4d30f0143e87f88c22e26b3d37f7bd
Chiramet Phong Penglerd <Phong@hotmail.ch>
2021-03-05 00:41:26 +0100

    add TODO idea

--- web/src/app/features/schedule/shared/state/booking.model.ts
+++ web/src/app/features/schedule/shared/state/booking.model.ts
@@ -2,6 +2,8 @@ import {ID} from '@datorama/akita';
 import {Project} from '../../../projects/state';
 import {Resource} from '../../../resources/state';
 
+
+// TODO maybe make it to Parse.Query<Object<Booking>> so we don't have to map and use relations easier
 export interface Booking {
   id: ID;
   start: number;
